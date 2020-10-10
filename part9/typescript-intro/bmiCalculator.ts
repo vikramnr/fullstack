e7232f98622e725ac30560d67ca8874fae2670e5
vikram <vikram1912@protonamil.com>
2020-10-10 09:50:00 +0530

    completed part9.3

--- part9/typescript-intro/bmiCalculator.ts
+++ part9/typescript-intro/bmiCalculator.ts
@@ -1,23 +1,52 @@
-const calculateBmi= (height: number, weight: number) :string  => {
-    let mHeight=  height/100
-    let bmi = weight/(mHeight*mHeight)
-    if(bmi<15) {
-        return 'Very severely underweight'
-    } else if(bmi<16 && bmi>=15) {
-        return 'Severely underweight	'
-    } else if (bmi>16 && bmi<18.5) {
-        return 'Underweight'
-    } else if(bmi>=18.5 && bmi<25 ) {
-        return 'Normal (healthy weight)'
-    } else if(bmi>=25 && bmi<30) {
-        return 'Overweight'
-    } else if(bmi>=30 && bmi<35) {
-        return 'Obese Class I (Moderately obese)'
-    } else if(bmi>=35 && bmi<40) {
-        return 'Obese Class II (Severely obese)'
-    } else if(bmi>=40) {
-        return 'Obese Class III (Very severely obese)'
-    }
+interface BmiValues {
+  height: number;
+  weight: number;
 }
 
-console.log(calculateBmi(180, 74))
+const calculateBmi = (bmiValues: BmiValues): string => {
+  let { height, weight } = bmiValues;
+
+  let mHeight = height / 100;
+  let bmi = weight / (mHeight * mHeight);
+  if (bmi < 15) {
+    return "Very severely underweight";
+  } else if (bmi < 16 && bmi >= 15) {
+    return "Severely underweight	";
+  } else if (bmi > 16 && bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi >= 25 && bmi < 30) {
+    return "Overweight";
+  } else if (bmi >= 30 && bmi < 35) {
+    return "Obese Class I (Moderately obese)";
+  } else if (bmi >= 35 && bmi < 40) {
+    return "Obese Class II (Severely obese)";
+  } else if (bmi >= 40) {
+    return "Obese Class III (Very severely obese)";
+  }
+};
+
+const parseArgumentsForBMI = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error("not enough arugments");
+  if (args.length > 6) throw new Error("too many arugments");
+
+  let argsValue1: number = Number(args[2]);
+  let argsValue2: number = Number(args[3]);
+
+  if (!isNaN(argsValue1) && !isNaN(argsValue2)) {
+    return {
+      height: argsValue1,
+      weight: argsValue2,
+    };
+  } else {
+      throw new Error('arugment should of type number.. Please check your input and try again')
+  }
+};
+
+try {
+  const bmiValues: BmiValues = parseArgumentsForBMI(process.argv);
+  console.log(calculateBmi(bmiValues));
+} catch (e) {
+  console.log("error occurend", e.message);
+}
--- part9/typescript-intro/exerciseCalculator.ts
+++ part9/typescript-intro/exerciseCalculator.ts
@@ -1,32 +1,66 @@
-interface Result { 
-    periodLength: number
-    trainingDays: number
-    success: boolean
-    rating: number
-    ratingDescription: string
-    target: number
-    average: number
-}
-const calculateExercises = (training:Array<number>,target: number):Result=> {
-    let trainedDays = training.filter(n => n>=target)
-    let rating = Math.ceil(training.reduce((a,r) => a+r,0)/7)
-    const ratingMapping : {[key:number]:string} = {
-        1 : 'good days are ahead, keep training', 
-        2 : 'not too bad but could be better',
-        3 : 'keep training, best is yet to come',
-        4 : 'push harder, just few more laps!!!',
-        5 : 'great workout, take some timeoff?'
-    }
-    
-    return { 
-        periodLength: training.length,
-        trainingDays: training.filter(n => n>0).length,
-        success: training.every(n => n>=target),
-        rating,
-        ratingDescription: ratingMapping[rating],
-        target,
-        average: trainedDays.reduce((a,r) => a+r,0)/training.length 
-    }
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1],2))
+interface ExerciseValues {
+  training: Array<number>;
+  target: number;
+}
+
+export const parseArgumentsForCalculator = (
+  args: Array<string>
+): ExerciseValues => {
+  if (args.length < 4) throw new Error("not enough arugments");
+  if (args.length > 12) throw new Error("too many arugments");
+
+  let exerciseValues: Array<number> = [...args.slice(2)].map(Number);
+
+  if (exerciseValues.every((n) => !isNaN(n)) && exerciseValues.length === 10) {
+    if (exerciseValues.length === 10) {
+      return {
+        training: exerciseValues.slice(1, exerciseValues.length),
+        target: exerciseValues[0],
+      };
+    } else {
+        throw new Error('arugments should be of type number... please check your input and try againg')
+    }
+  }
+};
+
+const calculateExercises = (exerciseValues: ExerciseValues): Result => {
+  let { training, target } = exerciseValues;
+  let trainedDays = training.filter((n) => n >= target);
+  let rating = Math.ceil(training.reduce((a, r) => a + r, 0) / 7);
+  const ratingMapping: { [key: number]: string } = {
+    1: "good days are ahead, keep training",
+    2: "not too bad but could be better",
+    3: "keep training, best is yet to come",
+    4: "push harder, just few more laps!!!",
+    5: "great workout, take some timeoff?",
+  };
+
+  return {
+    periodLength: training.length,
+    trainingDays: training.filter((n) => n > 0).length,
+    success: training.every((n) => n >= target),
+    rating,
+    ratingDescription: ratingMapping[rating],
+    target,
+    average: trainedDays.reduce((a, r) => a + r, 0) / training.length,
+  };
+};
+
+try {
+  let exerciseValues: ExerciseValues = parseArgumentsForCalculator(
+    process.argv
+  );
+  console.log(calculateExercises(exerciseValues));
+} catch (e) {
+  console.log("error occurend", e.message);
+}
