efcb2f794c2ba6a35a91f1b8d315f82a1441bc73
vikram <vikram1912@protonamil.com>
2020-10-25 18:46:15 +0530

    added front-end compeleted part 9.9

--- part9/patientor/index.ts
+++ part9/patientor/index.ts
@@ -1,13 +1,18 @@
-import express from 'express'
-const app = express()
-app.use(express.json())
+import express from 'express';
+import cors from 'cors';
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/ping',(_req,res) => {
-    res.send('pong')
-})
+app.get('/api/ping',(_req,res) => {
+    res.send('pong');
+});
 
-const PORT = 3000
+app.get('/api/patients',(_req,res) => {
+    res.send('hello');
+});
+const PORT = 3001;
 
 app.listen(PORT,() => {
-    console.log(`app started at ${PORT}`)
-})
+    console.log(`app started at ${PORT}`);
+});
